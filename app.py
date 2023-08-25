548d2e736ae51f793b3d041d45296e86f28949d2
kzri4 <kazu4126ab@gmail.com>
2023-08-25 10:42:17 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,53 @@
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.environ["ACCESS_TOKEN"])
+handler = WebhookHandler(os.environ["CHANNEL_SECRET"])
+
+
+@app.route('/')
+def index():
+    return 'You call index()'
+
+
+@app.route("/push_sample")
+def push_sample():
+    """プッシュメッセージを送る"""
+    user_id = os.environ["USER_ID"]
+    line_bot_api.push_message(user_id, TextSendMessage(text="Hello World!"))
+
+    return "OK"
+
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    """Messaging APIからの呼び出し関数"""
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError as e:
+        abort(400)
+
+    return "OK"
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(event.reply_token,
+                               TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
